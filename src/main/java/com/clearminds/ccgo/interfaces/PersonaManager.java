5b9020727c71116bebb85a73d7da913cc9186a56
CristianGomezO <cristiangomezosorio17@gmail.com>
2020-12-10 11:16:25 -0500

    TestManager funcionando con Classforname en properties. Agrego evidencia en el respectivo repositorio

--- src/main/java/com/clearminds/ccgo/interfaces/PersonaManager.java
+++ src/main/java/com/clearminds/ccgo/interfaces/PersonaManager.java
@@ -1,20 +1,32 @@
 package com.clearminds.ccgo.interfaces;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import com.clearminds.ccgo.exceptions.InstanceException;
 import com.clearminds.ccgo.model.Persona;
 
 public class PersonaManager {
 	private ServicioPersona serv;
 
-	public PersonaManager() throws InstanceException{
+	public PersonaManager() throws InstanceException {
+		Properties propiedades = new Properties();
+		File f = new File("properties");
+		System.out.println("ruta:" + f.getAbsoluteFile());
+		String propiedadClase = "";
 		try {
-			Class claseBDD = Class.forName("com.clearminds.ccgo.interfaces.ServicioPersonaBDD");
-			
-			serv=(ServicioPersona)claseBDD.newInstance();
+			propiedades.load(new FileInputStream(
+					"C:/Users/USER/Desktop/Herencia/src/main/java/com/clearminds/ccgo/middleware/properties"));
+			propiedadClase =  propiedades.getProperty("clase");
+			Class claseBDD = Class.forName(propiedadClase);
+
+			serv = (ServicioPersona) claseBDD.newInstance();
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
 		}
+
 	}
 
 	public void insertarPersona(Persona persona) {
