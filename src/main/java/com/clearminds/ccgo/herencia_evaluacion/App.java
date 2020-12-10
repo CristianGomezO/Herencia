e9219142391147b1cd57b865120ca43a5752f2fc
CristianGomezO <cristiangomezosorio17@gmail.com>
2020-12-10 10:11:19 -0500

    Empezando con la parte 3 de la evaluación

---
+++ src/main/java/com/clearminds/ccgo/herencia_evaluacion/App.java
@@ -0,0 +1,13 @@
+package com.clearminds.ccgo.herencia_evaluacion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
